e229d1ccd440b7c899ef9c63f245ee60bf87cbe7
Muyinda Rogers <moverr@gmail.com>
2020-01-21 17:26:00 +0300

    Debugging

--- admin/src/containers/HomePage/index.js
+++ admin/src/containers/HomePage/index.js
@@ -14,11 +14,9 @@ import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
 import ListGroup from 'react-bootstrap/ListGroup';
 
-import {
-  Route,
-  NavLink,
-  BrowserRouter
-} from "react-router-dom";
+import {  Route,NavLink,  BrowserRouter} from "react-router-dom";
+import ApexCharts from 'apexcharts';
+
 
 
 
@@ -29,20 +27,30 @@ export default class componentName extends Component {
   }
  
 
+    options = {
+    chart: {
+      type: 'line'
+    },
+    series: [{
+      name: 'sales',
+      data: [30,40,35,50,49,60,70,91,125]
+    }],
+    xaxis: {
+      categories: [1991,1992,1993,1994,1995,1996,1997, 1998,1999]
+    }
+  }
+
   
 
   render() {
     return (
       <div>
-        <div
-          style={{
-            width: '400px',
-            height: '300px'
-          }}
-        >
+        <div    style={{  width: '400px',  height: '300px' }} >
           <h1> MOVER</h1>
-
-         
+           {
+             chart = new ApexCharts(document.querySelector("#chart"), options);
+             chart.render();                      
+           } 
         </div>
       </div>
     )
