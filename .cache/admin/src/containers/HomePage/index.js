62aeba3433e7b364ca7edaa971e66f30a84d3379
Muyinda Rogers <moverr@gmail.com>
2020-01-21 18:56:25 +0300

    :Debugging :

--- .cache/admin/src/containers/HomePage/index.js
+++ .cache/admin/src/containers/HomePage/index.js
@@ -1,61 +1,23 @@
 import React, { Component } from 'react';
-import { Chart } from 'react-charts'
-
-
-
-
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
-import Container from 'react-bootstrap/Container';
-import ListGroup from 'react-bootstrap/ListGroup';
-
-import { Route, NavLink, BrowserRouter } from "react-router-dom";
-import ApexCharts from 'apexcharts';
-
-
-
-
+ 
 
 export default class componentName extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props)
   }
 
-
-  options = {
-    chart: {
-      type: 'line'
-    },
-    series: [{
-      name: 'sales',
-      data: [30, 40, 35, 50, 49, 60, 70, 91, 125]
-    }],
-    xaxis: {
-      categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
-    }
-  }
-
-  renderChart = () => {
-    let chart = new ApexCharts(document.querySelector("#chart"), options)
-
-    chart.render();
-  }
+   
 
   render() {
-
     return (
       <div>
-        <div style={{ width: '400px', height: '300px' }} >
-          <h1> MOVER</h1>
-          {
-            this.renderChart()
-
-          }
+        <div
+          style={{
+            width: '400px',
+            height: '300px'
+          }}
+        >
+         <h1> HOME SWEAT HOME </h1>
         </div>
       </div>
     )
