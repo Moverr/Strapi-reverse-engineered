01f49b28bc526a34fa5f06688a36d2735031581c
Muyinda Rogers <moverr@gmail.com>
2020-01-21 17:32:34 +0300

    :Deb

--- .cache/admin/src/containers/HomePage/index.js
+++ .cache/admin/src/containers/HomePage/index.js
@@ -14,72 +14,8 @@ import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
 import ListGroup from 'react-bootstrap/ListGroup';
 
-import {
-  Route,
-  NavLink,
-  BrowserRouter
-} from "react-router-dom";
-
-import ColumnChartwithIndexlabel from "../../charts/overview/Column Chart with Indexlabel";
-import ChartWithCrosshair from "../../charts/overview/Chart with Crosshair";
-import ChartWithZoom from "../../charts/overview/Chart with Zoom";
-import AnimatedChart from "../../charts/overview/Animated Chart";
-import MultiseriesChart from "../../charts/overview/Multiseries Chart";
-import MultipleAxisChart from "../../charts/overview/Multiple Axis Chart";
-import ChartWithScaleBreaks from "../../charts/overview/Chart with Scale Breaks";
-import ChartWithLogarithmicAxis from "../../charts/overview/Chart with Logarithmic Axis";
-import PerformanceDemo from "../../charts/overview/Performance Demo";
-import ChartWithDrillDown from "../../charts/overview/Chart with Drilldown";
-
-import LineChart from "../../charts/line charts/Line Chart";
-import SplineChart from "../../charts/line charts/Spline Chart";
-import StepLineChart from "../../charts/line charts/Step Line Chart";
-
-import AreaChart from "../../charts/area charts/Area Chart";
-import MultiSeriesAreaChart from "../../charts/area charts/Multi Series Area Chart";
-import SplineAreaChart from "../../charts/area charts/Spline Area Chart";
-import StepAreaChart from "../../charts/area charts/Step Area Chart";
-import RangeAreaChart from "../../charts/area charts/Range Area Chart";
-import RangeSplineAreaChart from "../../charts/area charts/Range Spline Area Chart";
-import StackedAreaChart from "../../charts/area charts/Stacked Area Chart";
-import StackedArea100Chart from "../../charts/area charts/Stacked Area 100 Chart";
-
-import ColumnChart from "../../charts/column charts/Column Chart";
-import BarChart from "../../charts/column charts/Bar Chart";
-import RangeColumnChart from "../../charts/column charts/Range Column Chart";
-import StackedColumnChart from "../../charts/column charts/Stacked Column Chart";
-import StackedColumn100Chart from "../../charts/column charts/Stacked Column 100 Chart";
-import RangeBarChart from "../../charts/column charts/Range Bar Chart";
-import StackedBarChart from "../../charts/column charts/Stacked Bar Chart";
-import StackedBar100Chart from "../../charts/column charts/Stacked Bar 100 Chart";
-import WaterfallChart from "../../charts/column charts/Waterfall Chart";
-
-import PieChart from "../../charts/pie & funnel charts/Pie Chart";
-import PieChartWithCustomization from "../../charts/pie & funnel charts/Pie Chart with Customization";
-import DoughnutChart from "../../charts/pie & funnel charts/Doughnut Chart";
-import FunnelChart from "../../charts/pie & funnel charts/Funnel Chart";
-import FunnelChartWithCustomNeck from "./pie & funnel charts/Funnel Chart with Custom Neck";
-import PyramidChart from "../../charts/pie & funnel charts/Pyramid Chart";
-
-import CandlestickChart from "../../charts/financial charts/Candlestick Chart";
-import OhlcChart from "../../charts/financial charts/OHLC Chart";
-
-import ScatterChart from "../../charts/scatter & bubble charts/Scatter Chart";
-import ScatterChartWithCustomMarkers from "./scatter & bubble charts/Scatter Chart with Custom Markers";
-import BubbleChart from "../../charts/scatter & bubble charts/Bubble Chart";
-
-import BoxAndWhiskerChart from "../../charts/box & whisker charts/Box & Whisker Chart";
-import BoxAndWhiskerChartWithCustomization from "../../charts/box & whisker charts/Box & Whisker Chart with Customization";
-
-import ErrorChart from "../../charts/combination charts/Error Chart";
-import ErrorLineChart from "../../charts/combination charts/Error Line Chart";
-import ParetoChart from "../../charts/combination charts/Pareto Chart";
-import ColumnLineAreaChart from "../../charts/combination charts/Column Line Area Chart";
-
-import DynamicLineChart from "../../charts/dynamic charts/Dynamic Line Chart";
-import DynamicColumnChart from "../../charts/dynamic charts/Dynamic Column Chart";
-import DynamicMultiSeriesChart from "../../charts/dynamic charts/Dynamic Multi Series Chart";
-
+import { Route, NavLink, BrowserRouter } from "react-router-dom";
+import ApexCharts from 'apexcharts';
 
 
 
@@ -91,46 +27,35 @@ export default class componentName extends Component {
   }
 
 
-  chart = () => {
+  options = {
+    chart: {
+      type: 'line'
+    },
+    series: [{
+      name: 'sales',
+      data: [30, 40, 35, 50, 49, 60, 70, 91, 125]
+    }],
+    xaxis: {
+      categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
+    }
+  }
 
-    const data = React.useMemo(
-      () => [
-        {
-          label: 'Series 1',
-          data: [[0, 1], [1, 2], [2, 4], [3, 2], [4, 7]]
-        },
-        {
-          label: 'Series 2',
-          data: [[0, 3], [1, 1], [2, 5], [3, 6], [4, 4]]
-        }
-      ],
-      []
-    );
+  renderChart = () => {
+    let chart = new ApexCharts(document.querySelector("#chart"), options)
 
-    const axes = React.useMemo(
-      () => [
-        { primary: true, type: 'linear', position: 'bottom' },
-        { type: 'linear', position: 'left' }
-      ],
-      []
-    );
-
-
-
+    chart.render();
   }
 
   render() {
+
     return (
       <div>
-        <div
-          style={{
-            width: '400px',
-            height: '300px'
-          }}
-        >
+        <div style={{ width: '400px', height: '300px' }} >
           <h1> MOVER</h1>
+          {
+            this.renderChart()
 
-          {/* <ColumnChartwithIndexlabel/> */}
+          }
         </div>
       </div>
     )
--- admin/src/containers/HomePage/index.js
+++ admin/src/containers/HomePage/index.js
@@ -15,8 +15,7 @@ import Container from 'react-bootstrap/Container';
 import ListGroup from 'react-bootstrap/ListGroup';
 
 import { Route, NavLink, BrowserRouter } from "react-router-dom";
-import ApexCharts from 'apexcharts';
-
+ 
 
 
 
@@ -25,37 +24,14 @@ export default class componentName extends Component {
   constructor() {
     super();
   }
-
-
-  options = {
-    chart: {
-      type: 'line'
-    },
-    series: [{
-      name: 'sales',
-      data: [30, 40, 35, 50, 49, 60, 70, 91, 125]
-    }],
-    xaxis: {
-      categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
-    }
-  }
-
-  renderChart = () => {
-    let chart = new ApexCharts(document.querySelector("#chart"), options)
-
-    chart.render();
-  }
-
+ 
   render() {
 
     return (
       <div>
         <div style={{ width: '400px', height: '300px' }} >
           <h1> MOVER</h1>
-          {
-            this.renderChart()
-
-          }
+           
         </div>
       </div>
     )
