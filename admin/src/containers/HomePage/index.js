6b8f9dd316ba3a89bd146b1b2cd000556bbab91f
Muyinda Rogers <moverr@gmail.com>
2020-01-21 15:25:10 +0300

    :Debugging :

--- admin/src/containers/HomePage/index.js
+++ admin/src/containers/HomePage/index.js
@@ -3,9 +3,11 @@ import React, { Component } from 'react'
 export default class componentName extends Component {
   render() {
     return (
-      <>
-        
-      </>
+     <div>
+       <div className={"md3"} >
+
+       </div>
+     </div>
     )
   }
 }
