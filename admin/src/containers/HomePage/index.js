b8d8620ebb2517457ba6c44ca84e7903dae3c168
Muyinda Rogers <moverr@gmail.com>
2020-01-21 15:35:29 +0300

    :Debugging :

--- admin/src/containers/HomePage/index.js
+++ admin/src/containers/HomePage/index.js
@@ -1,13 +1,51 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Chart } from 'react-charts'
+
 
 export default class componentName extends Component {
+  constructor() {
+
+  }
+  chart = () => {
+
+    const data = React.useMemo(
+      () => [
+        {
+          label: 'Series 1',
+          data: [[0, 1], [1, 2], [2, 4], [3, 2], [4, 7]]
+        },
+        {
+          label: 'Series 2',
+          data: [[0, 3], [1, 1], [2, 5], [3, 6], [4, 4]]
+        }
+      ],
+      []
+    );
+
+    const axes = React.useMemo(
+      () => [
+        { primary: true, type: 'linear', position: 'bottom' },
+        { type: 'linear', position: 'left' }
+      ],
+      []
+    );
+
+
+
+  }
+
   render() {
     return (
-     <div>
-       <div className={"md3"} >
-
-       </div>
-     </div>
+      <div>
+        <div
+          style={{
+            width: '400px',
+            height: '300px'
+          }}
+        >
+          <Chart data={data} axes={axes} />
+        </div>
+      </div>
     )
   }
 }
